accdb22147026423b375ef1974c5e30c833c32e0
Abraham Jimah Zorwi <77986280+iamjimah@users.noreply.github.com>
2023-06-30 01:07:21 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
       className="bg-success  d-flex flex-column justify-content-center align-items-center"
       style={{ height: "100vh" }}
     >
-      <div className="container">
+      <div className="container d-flex flex-column align-items-center justify-content-center">
         <h1>Home Page</h1>
         <h2>
           This site gives you access to all the information <br />
